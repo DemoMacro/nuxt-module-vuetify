6bbe7bbec00431e8cea2bc793ea1913f653e6f6e
Demo Macro <abc@imst.xyz>
2022-05-27 04:19:38 +0800

    fix: remove extra types

--- packages/nuxt-module-vuetify/src/runtime/plugin.mjs
+++ packages/nuxt-module-vuetify/src/runtime/plugin.mjs
@@ -1,5 +1,4 @@
 import { defineNuxtPlugin } from "nuxt/app";
-import { Plugin } from "@vue/runtime-core";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
@@ -7,7 +6,7 @@ import * as directives from "vuetify/directives";
 const options = JSON.parse(`<%= JSON.stringify(options) %>`);
 
 export default defineNuxtPlugin((nuxtApp) => {
-  const vuetify: Plugin = createVuetify({
+  const vuetify = createVuetify({
     components,
     directives,
     ...options,
